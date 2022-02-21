6b44d82e895948966d9ad212d73ed4eb84de922a
Burak Görez <77239106+paradass@users.noreply.github.com>
2022-02-21 09:05:04 +0300

    Güncelleme

--- Magra_Jam4/Assets/Scripts/Grid/GridMouseEvent.cs
+++ Magra_Jam4/Assets/Scripts/Grid/GridMouseEvent.cs
@@ -43,49 +43,35 @@ public class GridMouseEvent : MonoBehaviour
 
     void BlokSuilet()
     {
-        if(ToolManager.Instance.seciliBlok == -1)
+        if (ToolManager.Instance.seciliBlok != -1 && ToolManager.Instance.blokMiktari[ToolManager.Instance.seciliBlok] > 0)
         {
-
-        }
-        else
-        {
-            if (ToolManager.Instance.blokMiktari[ToolManager.Instance.seciliBlok] > 0)
+            suilet = Instantiate(bloklar[ToolManager.Instance.seciliBlok], new Vector3(transform.position.x, transform.position.y, -1), transform.rotation);
+            suilet.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(0, 0, 100, 0.3f);
+            suilet.GetComponent<Rigidbody2D>().gravityScale = 0;
+            //suilet.GetComponent<Rigidbody2D>().isKinematic = true;
+            try
             {
-                suilet = Instantiate(bloklar[ToolManager.Instance.seciliBlok], new Vector3(transform.position.x, transform.position.y, -1), transform.rotation);
-                suilet.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(0, 0, 100, 0.3f);
-                suilet.GetComponent<Rigidbody2D>().gravityScale = 0;
-                //suilet.GetComponent<Rigidbody2D>().isKinematic = true;
-                try
-                {
-                    Destroy(suilet.GetComponent<PolygonCollider2D>());
-                }
-                catch { }
-
-                try
-                {
-                    Destroy(suilet.GetComponent<BoxCollider2D>());
-                }
-                catch { }
+                Destroy(suilet.GetComponent<PolygonCollider2D>());
             }
+            catch { }
+
+            try
+            {
+                Destroy(suilet.GetComponent<BoxCollider2D>());
+            }
+            catch { }
         }
 
     }
 
     void BlokYerlestir()
     {
-        if(ToolManager.Instance.seciliBlok == -1)
+        if (ToolManager.Instance.seciliBlok != -1 &&  ToolManager.Instance.blokMiktari[ToolManager.Instance.seciliBlok] > 0)
         {
-
-        }
-        else
-        {
-            if (ToolManager.Instance.blokMiktari[ToolManager.Instance.seciliBlok] > 0)
-            {
-                ToolManager.Instance.blokMiktari[ToolManager.Instance.seciliBlok] -= 1;
-                obje = Instantiate(bloklar[ToolManager.Instance.seciliBlok], new Vector3(transform.position.x, transform.position.y, -1), transform.rotation);
-                obje.GetComponent<Rigidbody2D>().gravityScale = 0;
-                //obje.GetComponent<Rigidbody2D>().isKinematic = true;
-            }
+            ToolManager.Instance.blokMiktari[ToolManager.Instance.seciliBlok] -= 1;
+            obje = Instantiate(bloklar[ToolManager.Instance.seciliBlok], new Vector3(transform.position.x, transform.position.y, -1), transform.rotation);
+            obje.GetComponent<Rigidbody2D>().gravityScale = 0;
+            //obje.GetComponent<Rigidbody2D>().isKinematic = true;
         }
     }
 }
